f93ee6a82fe2485824f42172d3f3372fd5d3df7d
rjohn172 <153451301+russcraftscode@users.noreply.github.com>
2025-02-07 15:17:24 -0800

    Added protons

--- AtomSimClassic.py
+++ AtomSimClassic.py
@@ -1,43 +1,139 @@
+import math
+import random
 import tkinter as tk
+import Particle
+
 
 root = tk.Tk()
 root.title("Particles in Classical Mechanics")
 
 # build the canvas
-canvas_width = 400
-canvas_height = 400
+canvas_width = 1000
+canvas_height = 1000
 canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg='white')
 canvas.pack()
 
-# prototyping stuff
-circle1 = canvas.create_oval(50, 50, 100, 100, fill='blue')
-circle2 = canvas.create_oval(300, 50, 350, 100, fill='red')
+# # prototyping stuff
+# circle1 = canvas.create_oval(50, 50, 100, 100, fill='blue')
+# circle2 = canvas.create_oval(300, 50, 350, 100, fill='red')
+# circle3 = canvas.create_oval(300, 50, 350, 100, fill='red')
 
-dx1, dy1 = 2, 2
-dx2, dy2 = -2, 3
+particles = []
+
+for id in range(10):
+    particles.append(Particle.Electron(id,
+                                       canvas.create_oval(50, 50, 100, 100, fill='blue'),
+                                       canvas_width,
+                                       canvas_height))
+    particles[-1].x_pos = canvas_width/2 + (random.random()-.5) * 300
+    particles[-1].y_pos = canvas_height/2 + (random.random()-.5) * 300
+    particles[-1].x_vel = random.random()
+    particles[-1].y_vel = random.random()
+
+for id in range(10,13):
+    particles.append(Particle.Proton(id,
+                                       canvas.create_oval(50, 50, 100, 100, fill='Red'),
+                                       canvas_width,
+                                       canvas_height))
+    particles[-1].x_pos = canvas_width/2 + (random.random()-.5) * 300
+    particles[-1].y_pos = canvas_height/2 + (random.random()-.5) * 300
+    particles[-1].x_vel = 0
+    particles[-1].y_vel = 0
 
 
+
+# p1 = Particle.Electron(1, circle1, canvas_width, canvas_height)
+# p2 = Particle.Electron(2, circle2, canvas_width, canvas_height)
+# p3 = Particle.Electron(3, circle3, canvas_width, canvas_height)
+#
+# particles.append(p1)
+# particles.append(p2)
+# particles.append(p3)
+#
+# p1.x_pos = canvas_width/2
+# p1.y_pos = canvas_height/2
+# p1.x_vel = 0
+# p1.y_vel = 0
+#
+# p2.x_pos = canvas_width/2 + 10
+# p2.y_pos = canvas_height/2 +10
+# p2.x_vel = 0
+# p2.y_vel = 0
+#
+# p3.x_pos = 0
+# p3.y_pos = canvas_height/2
+# p3.x_vel = 1
+# p3.y_vel = 0
+
+
+
+def em_interact(p_a, p_b):
+    #make sure the particles have not interacted already or are interacting with themselves
+    if p_a.id not in p_b.interacted_with and p_a.id != p_b.id:
+        p_a.interacted_with.append(p_b.id)
+        p_b.interacted_with.append(p_a.id)
+        dist_x = abs(p_a.x_pos - p_b.x_pos)
+        dist_y = abs(p_a.y_pos - p_b.y_pos)
+        distance = math.sqrt(pow(dist_x,2) + pow(dist_y, 2))
+
+        # negative force is repulsion, positive force is attraction
+        force = 0
+        if distance > 0:
+            force = (p_a.electric_charge * p_b.electric_charge) / pow(distance, 2)
+
+        # calc total acceleration on the particles
+        p_a_accel = force/p_a.mass
+        p_b_accel = force / p_b.mass
+        #print(f"{distance=} a {p_a_accel} b {p_b_accel}") # DEBUG
+        # break down acceleration in x,y vectors
+        p_a_x_vector = 0
+        p_b_x_vector = 0
+        p_b_y_vector = 0
+
+        if (p_a.x_pos > p_b.x_pos):
+            p_a_x_vector = (dist_x/distance) * p_a_accel
+            p_b_x_vector = -(dist_x/distance) * p_b_accel
+        else:
+            p_a_x_vector = -(dist_x/distance) * p_a_accel
+            p_b_x_vector = (dist_x/distance) * p_b_accel
+        if (p_a.y_pos > p_b.y_pos):
+            p_a_y_vector = (dist_y/distance) * p_a_accel
+            p_b_y_vector = -(dist_y/distance) * p_b_accel
+        else:
+            p_a_y_vector = -(dist_y/distance) * p_a_accel
+            p_b_y_vector = (dist_y/distance) * p_b_accel
+        # apply acceleration
+        p_a.accel(p_a_x_vector, p_a_y_vector )
+        p_b.accel(p_b_x_vector, p_b_y_vector)
 def move_circles():
-    global dx1, dy1, dx2, dy2
+    #global p1, p2
 
-    canvas.move(circle1, dx1, dy1)
-    canvas.move(circle2, dx2, dy2)
+    for p_a in particles:
+        for p_b in particles:
+            em_interact(p_a, p_b)
 
-    x1, y1, x2, y2 = canvas.coords(circle1)
-    a1, b1, a2, b2 = canvas.coords(circle2)
+    for p in particles:
+        p.move()
+        #print(f"{p.x_vel},{p.y_vel}")
+        canvas.coords(p.shape, p.x_pos-p.radius,  p.y_pos-p.radius,
+                      p.x_pos+p.radius,  p.y_pos+p.radius)
+        p.reset()
 
-    if x1 <= 0 or x2 >= canvas_width:
-        dx1 = -dx1
-    if y1 <= 0 or y2 >= canvas_height:
-        dy1 = -dy1
 
-    if a1 <= 0 or a2 >= canvas_width:
-        dx2 = -dx2
-    if b1 <= 0 or b2 >= canvas_height:
-        dy2 = -dy2
+
+    #print(f"{p1.x_pos},{p1.y_pos} {p2.x_pos},{p2.y_pos}")
+
 
     # TODO replace prototyping execution of next frame
     root.after(20, move_circles)
 
 
+move_circles()
 root.mainloop()
+
+
+
+
+
+
+
--- Particle.py
+++ Particle.py
@@ -1,6 +1,7 @@
 class Electron:
-    def __init__(self, sim_width, sim_height):
-        self.radius = 1
+    def __init__(self, id,  shape, sim_width, sim_height):
+        self.id = id
+        self.radius = 5
         self.x_pos = 0
         self.y_pos = 0
         self.x_vel = 0
@@ -8,8 +9,19 @@ class Electron:
         self.x_acc = 0
         self.y_acc = 0
 
+        self.mass = .001
+        self.electric_charge = -1
+
         self.sim_width = sim_width
         self.sim_height = sim_height
+        self.shape = shape
+
+        self.interacted_with = [] #keeps track of what particles this has interacted with
+
+    def reset(self):
+        self.interacted_with.clear()
+        self.x_acc = 0
+        self.y_acc = 0
 
     def move(self):
         self.x_vel += self.x_acc
@@ -28,3 +40,56 @@ class Electron:
         if self.y_pos > self.sim_height:
             self.y_pos = self.sim_height
             self.y_vel = -self.y_vel
+
+
+    def accel(self, x, y):
+        self.x_acc += x
+        self.y_acc += y
+
+class Proton:
+    def __init__(self, id,  shape, sim_width, sim_height):
+        self.id = id
+        self.radius = 10
+        self.x_pos = 0
+        self.y_pos = 0
+        self.x_vel = 0
+        self.y_vel = 0
+        self.x_acc = 0
+        self.y_acc = 0
+
+        self.mass = .1
+        self.electric_charge = 1
+
+        self.sim_width = sim_width
+        self.sim_height = sim_height
+        self.shape = shape
+
+        self.interacted_with = [] #keeps track of what particles this has interacted with
+
+    def reset(self):
+        self.interacted_with.clear()
+        self.x_acc = 0
+        self.y_acc = 0
+
+    def move(self):
+        self.x_vel += self.x_acc
+        self.x_pos += self.x_vel
+        self.y_vel += self.y_acc
+        self.y_pos += self.y_vel
+        if self.x_pos < 0:
+            self.x_pos = 0
+            self.x_vel = -self.x_vel
+        if self.x_pos > self.sim_width:
+            self.x_pos = self.sim_width
+            self.x_vel = -self.x_vel
+        if self.y_pos < 0:
+            self.y_pos = 0
+            self.y_vel = -self.y_vel
+        if self.y_pos > self.sim_height:
+            self.y_pos = self.sim_height
+            self.y_vel = -self.y_vel
+
+
+    def accel(self, x, y):
+        self.x_acc += x
+        self.y_acc += y
