7ca48c95478f74c63aec201a53d257b1240f5fa2
russcraftscode <153451301+russcraftscode@users.noreply.github.com>
2025-02-10 11:15:09 -0800

    Combined all particles into single Partical class.

--- AtomSimClassic.py
+++ AtomSimClassic.py
@@ -3,136 +3,119 @@ import random
 import tkinter as tk
 import Particle
 
-
 root = tk.Tk()
 root.title("Particles in Classical Mechanics")
 
 # build the canvas
-canvas_width = 1000
-canvas_height = 800
-electron_count = 5
-proton_count = 5
+canvas_width = 800
+canvas_height = 600
+electron_count = 30
+proton_count = 10
+neutron_count = 10
 canvas = tk.Canvas(root, width=canvas_width, height=canvas_height, bg='white')
 canvas.pack()
 
-# # prototyping stuff
-# circle1 = canvas.create_oval(50, 50, 100, 100, fill='blue')
-# circle2 = canvas.create_oval(300, 50, 350, 100, fill='red')
-# circle3 = canvas.create_oval(300, 50, 350, 100, fill='red')
-
 particles = []
+nuclei = []
 
-for id in range(electron_count):
-    particles.append(Particle.Electron(id,
+id = 0;
+for p in range(electron_count):
+    particles.append(Particle.Particle(id, 'e',
                                        canvas.create_oval(50, 50, 100, 100, fill='blue'),
                                        canvas_width,
                                        canvas_height))
-    particles[-1].x_pos = canvas_width/2 + (random.random()-.5) * 300
-    particles[-1].y_pos = canvas_height/2 + (random.random()-.5) * 300
-    particles[-1].x_vel = 0 # random.random()
-    particles[-1].y_vel = 0 # random.random()
+    particles[-1].x_pos = canvas_width / 2 + (random.random() - .5) * 300
+    particles[-1].y_pos = canvas_height / 2 + (random.random() - .5) * 300
+    particles[-1].x_vel =  random.randint(-2,2)
+    particles[-1].y_vel = random.randint(-2,2)
+    id += 1
 
-for id in range(electron_count, proton_count+electron_count):
-    particles.append(Particle.Proton(id,
+for p in range(electron_count, proton_count + electron_count):
+    particles.append(Particle.Particle(id, 'p',
                                        canvas.create_oval(50, 50, 100, 100, fill='Red'),
                                        canvas_width,
                                        canvas_height))
-    particles[-1].x_pos = canvas_width/2 + (random.random()-.5) * 400
-    particles[-1].y_pos = canvas_height/2 + (random.random()-.5) * 400
-    particles[-1].x_vel = 0
-    particles[-1].y_vel = 0
+    particles[-1].x_pos = canvas_width / 2 + (random.random() - .5) * 400
+    particles[-1].y_pos = canvas_height / 2 + (random.random() - .5) * 400
+    particles[-1].x_vel = random.randint(-2,2)
+    particles[-1].y_vel = random.randint(-2,2)
+    id += 1
 
-
-
-# p1 = Particle.Electron(1, circle1, canvas_width, canvas_height)
-# p2 = Particle.Electron(2, circle2, canvas_width, canvas_height)
-# p3 = Particle.Electron(3, circle3, canvas_width, canvas_height)
-#
-# particles.append(p1)
-# particles.append(p2)
-# particles.append(p3)
-#
-# p1.x_pos = canvas_width/2
-# p1.y_pos = canvas_height/2
-# p1.x_vel = 0
-# p1.y_vel = 0
-#
-# p2.x_pos = canvas_width/2 + 10
-# p2.y_pos = canvas_height/2 +10
-# p2.x_vel = 0
-# p2.y_vel = 0
-#
-# p3.x_pos = 0
-# p3.y_pos = canvas_height/2
-# p3.x_vel = 1
-# p3.y_vel = 0
-
+for p in range(electron_count, proton_count + electron_count):
+    particles.append(Particle.Particle(id, 'n',
+                                       canvas.create_oval(50, 50, 100, 100, fill='Yellow'),
+                                       canvas_width,
+                                       canvas_height))
+    particles[-1].x_pos = canvas_width / 2 + (random.random() - .5) * 400
+    particles[-1].y_pos = canvas_height / 2 + (random.random() - .5) * 400
+    particles[-1].x_vel = random.randint(-2,2)
+    particles[-1].y_vel = random.randint(-2,2)
+    id += 1
 
 
 def em_interact(p_a, p_b):
-    #make sure the particles have not interacted already or are interacting with themselves
+    # make sure the particles have not interacted already or are interacting with themselves
     if p_a.id not in p_b.interacted_with and p_a.id != p_b.id:
         p_a.interacted_with.append(p_b.id)
         p_b.interacted_with.append(p_a.id)
         dist_x = abs(p_a.x_pos - p_b.x_pos)
         dist_y = abs(p_a.y_pos - p_b.y_pos)
-        distance = math.sqrt(pow(dist_x,2) + pow(dist_y, 2))
+        distance = math.sqrt(pow(dist_x, 2) + pow(dist_y, 2))
 
         # negative force is repulsion, positive force is attraction
         force = 0
         if distance > 0:
             force = (p_a.electric_charge * p_b.electric_charge) / pow(distance, 2)
             # calc total acceleration on the particles
-            p_a_accel = force/p_a.mass
+            p_a_accel = force / p_a.mass
             p_b_accel = force / p_b.mass
-            #print(f"{distance=} a {p_a_accel} b {p_b_accel}") # DEBUG
+            # print(f"{distance=} a {p_a_accel} b {p_b_accel}") # DEBUG
             # break down acceleration in x,y vectors
             p_a_x_vector = 0
             p_b_x_vector = 0
             p_b_y_vector = 0
 
             if (p_a.x_pos > p_b.x_pos):
-                p_a_x_vector = (dist_x/distance) * p_a_accel
-                p_b_x_vector = -(dist_x/distance) * p_b_accel
+                p_a_x_vector = (dist_x / distance) * p_a_accel
+                p_b_x_vector = -(dist_x / distance) * p_b_accel
             else:
-                p_a_x_vector = -(dist_x/distance) * p_a_accel
-                p_b_x_vector = (dist_x/distance) * p_b_accel
+                p_a_x_vector = -(dist_x / distance) * p_a_accel
+                p_b_x_vector = (dist_x / distance) * p_b_accel
             if (p_a.y_pos > p_b.y_pos):
-                p_a_y_vector = (dist_y/distance) * p_a_accel
-                p_b_y_vector = -(dist_y/distance) * p_b_accel
+                p_a_y_vector = (dist_y / distance) * p_a_accel
+                p_b_y_vector = -(dist_y / distance) * p_b_accel
             else:
-                p_a_y_vector = -(dist_y/distance) * p_a_accel
-                p_b_y_vector = (dist_y/distance) * p_b_accel
+                p_a_y_vector = -(dist_y / distance) * p_a_accel
+                p_b_y_vector = (dist_y / distance) * p_b_accel
             # apply acceleration
-            p_a.accel(p_a_x_vector, p_a_y_vector )
+            p_a.accel(p_a_x_vector, p_a_y_vector)
             p_b.accel(p_b_x_vector, p_b_y_vector)
+
+
 def move_circles():
-    #global p1, p2
+    # global p1, p2
+
+    # find all nuclei
+    global nuclei
+    nuclei = []
+    for p in particles:
+        if p.type == 'p':
+            nuclei.append(Particle.Nucleus(p.x_pos, p.y_pos))
+
+    # find closest nuclei
+    for p in particles:
+        if p.type == 'e':
+            p.find_closes_nucleus(nuclei)
 
     for p_a in particles:
         for p_b in particles:
             em_interact(p_a, p_b)
 
-    for e in range (electron_count):
-        closest_range = 1000000
-        for p in range (electron_count, proton_count + electron_count):
-            current_range = math.sqrt((particles[p].x_pos-particles[e].x_pos)**2 + (particles[p].y_pos-particles[e].y_pos)**2)
-            if current_range < closest_range:
-                closest_range = current_range
-                particles[e].closest_nucleus_x = particles[p].x_pos
-                particles[e].closest_nucleus_y = particles[p].y_pos
-
     for p in particles:
         p.move()
-        #print(f"{p.x_vel},{p.y_vel}")
-        canvas.coords(p.shape, p.x_pos-p.radius,  p.y_pos-p.radius,
-                      p.x_pos+p.radius,  p.y_pos+p.radius)
+        canvas.coords(p.shape, p.x_pos - p.radius, p.y_pos - p.radius,
+                      p.x_pos + p.radius, p.y_pos + p.radius)
         p.reset()
-
-
-
-    #print(f"{p1.x_pos},{p1.y_pos} {p2.x_pos},{p2.y_pos}")
-
 
     # TODO replace prototyping execution of next frame
     root.after(20, move_circles)
@@ -140,10 +123,3 @@ def move_circles():
 
 move_circles()
 root.mainloop()
-
-
-
-
-
-
-
--- Particle.py
+++ Particle.py
@@ -1,10 +1,36 @@
 import math
 
+class Nucleus:
+    def __init__(self, x, y ):
+        self.x_pos = x
+        self.y_pos = y
+        self.members = []
 
-class Electron:
-    def __init__(self, id, shape, sim_width, sim_height):
+class Particle:
+    def __init__(self, id, type, shape, sim_width, sim_height):
+        self.type = type
         self.id = id
-        self.radius = 5
+        if type == "p": # proton
+            self.radius = 5
+            self.mass = 1
+            self.electric_charge = 1
+            self.strong_charge = 1
+            self.energy_level = 0
+
+        if type == "n": # neutron
+            self.radius = 5
+            self.mass = 1
+            self.electric_charge = 0
+            self.strong_charge = 0
+            self.energy_level = 0
+
+        if type == "e": # neutron
+            self.radius = 1
+            self.mass = .001
+            self.electric_charge = -1
+            self.strong_charge = 0
+            self.energy_level = 1
+
         self.x_pos = 0
         self.y_pos = 0
         self.x_vel = 0
@@ -12,23 +38,30 @@ class Electron:
         self.x_acc = 0
         self.y_acc = 0
 
-        self.mass = .001
-        self.electric_charge = -1
-
         self.sim_width = sim_width
         self.sim_height = sim_height
         self.shape = shape
 
         self.interacted_with = []  #keeps track of what particles this has interacted with
 
-        self.closest_nucleus_x = 0
-        self.closest_nucleus_y = 0
-        self.energy_level = 1
+        self.closest_nucleus = None
 
     def reset(self):
         self.interacted_with.clear()
         self.x_acc = 0
         self.y_acc = 0
+        self.closest_nucleus = None
+
+    def find_closes_nucleus(self, nucleus_list):
+        if(nucleus_list):
+            closest_range = 1000000
+            for n in nucleus_list:
+                dist_to_nucleus_x = n.x_pos - self.x_pos
+                dist_to_nucleus_y = n.y_pos - self.y_pos
+                dist_to_nucleus = math.sqrt(dist_to_nucleus_x ** 2 + dist_to_nucleus_y ** 2)
+                if dist_to_nucleus < closest_range:
+                    closest_range = dist_to_nucleus
+                    self.closest_nucleus = n
 
     def move(self):
         MAX_SPEED = 5
@@ -46,20 +79,24 @@ class Electron:
         self.y_pos += self.y_vel
 
         # keep electron far enough away from the nucleus
-        dist_to_nucleus_x = self.closest_nucleus_x - self.x_pos
-        dist_to_nucleus_y = self.closest_nucleus_y - self.y_pos
-        dist_to_nucleus = math.sqrt(dist_to_nucleus_x ** 2 + dist_to_nucleus_y ** 2)
-        min_distance = 30 * self.energy_level
-        #print(f"{dist_to_nucleus=} {dist_to_nucleus_x=} {dist_to_nucleus_y=}" )
-        if dist_to_nucleus > 0:
-            if dist_to_nucleus < min_distance:
-                min_distance_factor = min_distance / dist_to_nucleus
-                dist_to_nucleus_x = dist_to_nucleus_x * min_distance_factor
-                dist_to_nucleus_y = dist_to_nucleus_y * min_distance_factor
-                self.x_pos = self.closest_nucleus_x - dist_to_nucleus_x
-                self.y_pos = self.closest_nucleus_y - dist_to_nucleus_y
-                #print(f"{min_distance_factor=} {dist_to_nucleus_x=} {dist_to_nucleus_y=}" )
+        #print(f"{self.type=} {self.closest_nucleus=}" )
+        if self.type == 'e' and self.closest_nucleus != None:
+            dist_to_nucleus_x = self.closest_nucleus.x_pos - self.x_pos
+            dist_to_nucleus_y = self.closest_nucleus.y_pos - self.y_pos
+            dist_to_nucleus = math.sqrt(dist_to_nucleus_x ** 2 + dist_to_nucleus_y ** 2)
+            #print(f"{self.closest_nucleus.x_pos=}, {self.closest_nucleus.y_pos=} {dist_to_nucleus_x=}, {dist_to_nucleus_y=}" )
+            min_distance = 30 * self.energy_level
+            #print(f"{dist_to_nucleus=} {dist_to_nucleus_x=} {dist_to_nucleus_y=}" )
+            if dist_to_nucleus > 0:
+                if dist_to_nucleus < min_distance:
+                    min_distance_factor = min_distance / dist_to_nucleus
+                    dist_to_nucleus_x = dist_to_nucleus_x * min_distance_factor
+                    dist_to_nucleus_y = dist_to_nucleus_y * min_distance_factor
+                    self.x_pos = self.closest_nucleus.x_pos - dist_to_nucleus_x
+                    self.y_pos = self.closest_nucleus.y_pos - dist_to_nucleus_y
+                    #print(f"{min_distance_factor=} {dist_to_nucleus_x=} {dist_to_nucleus_y=}" )
 
+        # bounce off walls
         if self.x_pos < 0:
             self.x_pos = 0
             self.x_vel = -self.x_vel
@@ -77,59 +114,3 @@ class Electron:
         self.x_acc += x
         self.y_acc += y
 
-
-class Proton:
-    def __init__(self, id, shape, sim_width, sim_height):
-        self.id = id
-        self.radius = 10
-        self.x_pos = 0
-        self.y_pos = 0
-        self.x_vel = 0
-        self.y_vel = 0
-        self.x_acc = 0
-        self.y_acc = 0
-
-        self.mass = .1
-        self.electric_charge = 1
-
-        self.sim_width = sim_width
-        self.sim_height = sim_height
-        self.shape = shape
-
-        self.interacted_with = []  #keeps track of what particles this has interacted with
-
-    def reset(self):
-        self.interacted_with.clear()
-        self.x_acc = 0
-        self.y_acc = 0
-
-    def move(self):
-        MAX_SPEED = 5
-        self.x_vel += self.x_acc
-        self.y_vel += self.y_acc
-
-        speed = math.sqrt(self.x_vel ** 2 + self.y_vel ** 2)
-        speed_reduction_factor = speed / MAX_SPEED
-        if speed_reduction_factor > 1:
-            self.x_vel = self.x_vel / speed_reduction_factor
-            self.y_vel = self.y_vel / speed_reduction_factor
-
-        self.x_pos += self.x_vel
-        self.y_pos += self.y_vel
-
-        if self.x_pos < 0:
-            self.x_pos = 0
-            self.x_vel = -self.x_vel
-        if self.x_pos > self.sim_width:
-            self.x_pos = self.sim_width
-            self.x_vel = -self.x_vel
-        if self.y_pos < 0:
-            self.y_pos = 0
-            self.y_vel = -self.y_vel
-        if self.y_pos > self.sim_height:
-            self.y_pos = self.sim_height
-            self.y_vel = -self.y_vel
-
-    def accel(self, x, y):
-        self.x_acc += x
-        self.y_acc += y
