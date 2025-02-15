5ced48f8ca0ba1fbfccfcd7b2f8e9a856046d7b7
rjohn172 <153451301+russcraftscode@users.noreply.github.com>
2025-02-15 12:14:52 -0800

    Add nuclear particle collisions

--- AtomSimClassic.py
+++ AtomSimClassic.py
@@ -5,13 +5,14 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Particles in Classical Mechanics")
 
-
 MAX_SPEED = 5
+HEAVY_RADIUS = 3
+MASSIVE_PARTICLE_STRONG_CHARGE = 8
+ELECTRON_RADIUS = 2
+FORCE_DIST_MODIFIER = 1
 electron_orbital_dist = 30
 nuclear_particle_spacing = 10
-# simple_orbitals = tk.BooleanVar(value = False)
-# strong_force_active = tk.BooleanVar(value = True)
-# electrostatic_force_active = tk.BooleanVar(value = True)
+
 simple_orbitals = tk.BooleanVar()
 simple_orbitals.set(True)
 strong_force_active = tk.BooleanVar()
@@ -30,21 +31,21 @@ class Particle:
         self.type = type
         self.id = id
         if type == "p":  # proton
-            self.radius = 5
+            self.radius = HEAVY_RADIUS
             self.mass = 1
             self.electrostatic_charge = 1
-            self.strong_charge = 1
+            self.strong_charge = MASSIVE_PARTICLE_STRONG_CHARGE
             self.energy_level = 0
 
         if type == "n":  # neutron
-            self.radius = 5
+            self.radius = HEAVY_RADIUS
             self.mass = 1
             self.electrostatic_charge = 0
-            self.strong_charge = 1
+            self.strong_charge = MASSIVE_PARTICLE_STRONG_CHARGE
             self.energy_level = 0
 
         if type == "e":  # electron
-            self.radius = 3
+            self.radius = ELECTRON_RADIUS
             self.mass = .001
             self.electrostatic_charge = -1
             self.strong_charge = 0
@@ -68,6 +69,7 @@ class Particle:
 
     def reset(self):
         self.electrostatic_interacted.clear()
+        self.strong_interacted.clear()
         self.x_acc = 0
         self.y_acc = 0
         self.closest_nucleus = None
@@ -82,7 +84,6 @@ class Particle:
                 if dist_to_nucleus < closest_range:
                     closest_range = dist_to_nucleus
                     self.closest_nucleus = n
-
 
     def move(self):
         #MAX_SPEED = 5
@@ -117,16 +118,14 @@ class Particle:
             self.y_pos = self.sim_height
             self.y_vel = -self.y_vel
 
-    def orbital_old(self):
+    def orbital_simple(self):
         # keep electron far enough away from the nucleus
         #print(f"{self.type=} {self.closest_nucleus=}" )
         if self.type == 'e' and self.closest_nucleus != None:
             dist_to_nucleus_x = self.closest_nucleus.x_pos - self.x_pos
             dist_to_nucleus_y = self.closest_nucleus.y_pos - self.y_pos
             dist_to_nucleus = math.sqrt(dist_to_nucleus_x ** 2 + dist_to_nucleus_y ** 2)
-            #print(f"{self.closest_nucleus.x_pos=}, {self.closest_nucleus.y_pos=} {dist_to_nucleus_x=}, {dist_to_nucleus_y=}" )
             min_distance = 30 * self.energy_level
-            #print(f"{dist_to_nucleus=} {dist_to_nucleus_x=} {dist_to_nucleus_y=}" )
             if dist_to_nucleus > 0:
                 if dist_to_nucleus < min_distance:
                     min_distance_factor = min_distance / dist_to_nucleus
@@ -139,7 +138,7 @@ class Particle:
     def orbital(self):
         global simple_orbitals
         if simple_orbitals.get():
-            self.orbital_old()
+            self.orbital_simple()
             return
         # keep electron far enough away from the nucleus
         if self.type == 'e' and self.closest_nucleus != None:
@@ -174,9 +173,6 @@ class Particle:
     def accel(self, x, y):
         self.x_acc += x
         self.y_acc += y
-
-
-
 
 
 def delete_all_particles():
@@ -304,16 +300,11 @@ def em_interact(p_a, p_b):
         # negative force is repulsion, positive force is attraction
         force = 0
         if distance > 0:
-            force = (p_a.electrostatic_charge * p_b.electrostatic_charge) / pow(distance, 2)
-            # calc total acceleration on the particles
+            force = (p_a.electrostatic_charge * p_b.electrostatic_charge) / pow(distance/FORCE_DIST_MODIFIER, 2)
+
+        # calc total acceleration on the particles
             p_a_accel = force / p_a.mass
             p_b_accel = force / p_b.mass
-            # print(f"{distance=} a {p_a_accel} b {p_b_accel}") # DEBUG
-            # break down acceleration in x,y vectors
-            p_a_x_vector = 0
-            p_b_x_vector = 0
-            p_b_x_vector = 0
-            p_b_y_vector = 0
 
             if (p_a.x_pos > p_b.x_pos):
                 p_a_x_vector = (dist_x / distance) * p_a_accel
@@ -331,67 +322,72 @@ def em_interact(p_a, p_b):
             p_a.accel(p_a_x_vector, p_a_y_vector)
             p_b.accel(p_b_x_vector, p_b_y_vector)
 
+
 def strong_interact(p_a, p_b):
     # make sure the particles have not interacted already or are interacting with themselves
-    if p_a.id not in p_b.electrostatic_interacted and p_a.id != p_b.id:
+    if p_a.id not in p_b.strong_interacted and p_a.id != p_b.id:
         min_spacing = p_a.radius + p_b.radius
-        p_a.electrostatic_interacted.append(p_b.id)
-        p_b.electrostatic_interacted.append(p_a.id)
-        dist_x = abs(p_a.x_pos - p_b.x_pos)
-        dist_y = abs(p_a.y_pos - p_b.y_pos)
-        distance = math.sqrt(pow(dist_x, 2) + pow(dist_y, 2))
+        p_a.strong_interacted.append(p_b.id)
+        p_b.strong_interacted.append(p_a.id)
+        dist_x = p_a.x_pos - p_b.x_pos
+        dist_y = p_a.y_pos - p_b.y_pos
+        distance = math.sqrt(dist_x ** 2 + dist_y ** 2)
+        norm_x = dist_x / distance
+        norm_y = dist_y / distance
 
         # all forces are negative because all the strong force interactions are attractive
         # with negative acceleration in the vector away from the other particle
-        force = 0
-        if distance > 0:
-            #if particles are not close, accelerate towards eachother
-            #if distance >= nuclear_particle_spacing:
-            if distance >= min_spacing:
+        if distance >= min_spacing:
+            force = -(p_a.strong_charge * p_b.strong_charge) / pow(distance/FORCE_DIST_MODIFIER, 4)
+            # calc total acceleration on the particles
+            p_a_accel = force / p_a.mass
+            p_b_accel = force / p_b.mass
+            p_a_x_vector = norm_x * p_a_accel
+            p_a_y_vector = norm_y * p_a_accel
+            p_b_x_vector = -norm_x * p_b_accel
+            p_b_y_vector = -norm_y * p_b_accel
+            # apply acceleration
+            p_a.accel(p_a_x_vector, p_a_y_vector)
+            p_b.accel(p_b_x_vector, p_b_y_vector)
 
-                force = -(p_a.strong_charge * p_b.strong_charge) / pow(distance, 3)
-                # calc total acceleration on the particles
-                p_a_accel = force / p_a.mass
-                p_b_accel = force / p_b.mass
-                # print(f"{distance=} a {p_a_accel} b {p_b_accel}") # DEBUG
-                # break down acceleration in x,y vectors
-                p_a_x_vector = 0
-                p_b_x_vector = 0
-                p_b_x_vector = 0
-                p_b_y_vector = 0
+        #if particles are too close, have them collide and exchange momentum
+        if 0 < distance < min_spacing:
+            # correct overlap
+            overlap_depth = min_spacing - distance
 
-                if (p_a.x_pos > p_b.x_pos):
-                    p_a_x_vector = (dist_x / distance) * p_a_accel
-                    p_b_x_vector = -(dist_x / distance) * p_b_accel
-                else:
-                    p_a_x_vector = -(dist_x / distance) * p_a_accel
-                    p_b_x_vector = (dist_x / distance) * p_b_accel
-                if (p_a.y_pos > p_b.y_pos):
-                    p_a_y_vector = (dist_y / distance) * p_a_accel
-                    p_b_y_vector = -(dist_y / distance) * p_b_accel
-                else:
-                    p_a_y_vector = -(dist_y / distance) * p_a_accel
-                    p_b_y_vector = (dist_y / distance) * p_b_accel
-                # apply acceleration
-                p_a.accel(p_a_x_vector, p_a_y_vector)
-                p_b.accel(p_b_x_vector, p_b_y_vector)
-                #print(f"{distance=} {nuclear_particle_spacing=} {force=}")
+            # move the particles to undo the overlap
+            move_x = norm_x * overlap_depth / 2
+            move_y = norm_y * overlap_depth / 2
+            p_a.x_pos += move_x
+            p_a.y_pos += move_y
+            p_b.x_pos -= move_x
+            p_b.y_pos -= move_y
 
-            #if particles are too close, have them collide and exchange momentum
-            #if distance < nuclear_particle_spacing:
-            if distance < min_spacing:
-                print(f"{p_a.x_vel=}, {p_a.y_vel=} {p_b.x_vel=}, {p_b.y_vel=}")
-                # save particle a's velocity in temp
-                temp_x_vel = p_a.x_vel
-                temp_y_vel = p_a.y_vel
-                # add particle b's velocity to particle a
-                p_a.x_vel += p_b.x_vel
-                p_a.y_vel -= p_b.y_vel
-                # add particle a's velocity to particle b using the stored values
-                p_b.x_vel += temp_x_vel
-                p_b.y_vel -= temp_y_vel
-                print(f"{p_a.x_vel=}, {p_a.y_vel=} {p_b.x_vel=}, {p_b.y_vel=}")
-                # stop acceleration due to impact
+            #slow partiles a bit to prevent spin of death
+            p_a.x_vel *= 1-abs(p_a.x_vel/MAX_SPEED)
+            p_a.y_vel *= 1-abs(p_b.y_vel/MAX_SPEED)
+            p_b.x_vel *= 1-abs(p_a.x_vel/MAX_SPEED)
+            p_b.y_vel *= 1-abs(p_b.y_vel/MAX_SPEED)
+
+            # determine relative velocities
+            d_x_vel = p_a.x_vel - p_b.x_vel
+            d_y_vel = p_a.y_vel - p_b.y_vel
+
+            # project the velocity on the normal
+            dot_vel = d_x_vel * norm_x + d_y_vel * norm_y
+
+            # if velocity is positive the particles are separating and no collision needs to happen
+            if dot_vel > 0: return
+
+            # calc impulse (impulse is delta momentum)
+            elasticity = 0
+            imp = ((1 + elasticity) * dot_vel) / (1 / p_a.mass + 1 / p_b.mass)
+
+            # apply acceleration
+            p_a.x_vel += -imp * p_b.mass * norm_x
+            p_a.y_vel += -imp * p_b.mass * norm_y
+            p_b.x_vel += imp * p_a.mass * norm_x
+            p_b.y_vel += imp * p_a.mass * norm_y
 
 
 
@@ -403,7 +399,6 @@ def update_particles():
         if p.type == 'p': proton_count += 1
         if p.type == 'e': electron_count += 1
         if p.type == 'n': neutron_count += 1
-    # TODO: Make this account for differing proton charges
     total_charge = proton_count - electron_count
     info_string = (f"Protons {proton_count}\nElectrons {electron_count}\n"
                    f"Neutrons{neutron_count}\nTotal Charge {total_charge}\n")
@@ -411,6 +406,7 @@ def update_particles():
     info_field.config(text=info_string)
 
     # find all nuclei
+    # TODO this only finds protons. Consider if it is worth it to define nuclei
     global nuclei
     nuclei = []
     for p in particles:
@@ -437,12 +433,8 @@ def update_particles():
         for p_a in particles:
             for p_b in particles:
                 if p_a.electrostatic_charge != 0 and p_b.electrostatic_charge != 0:
-                 em_interact(p_a, p_b)
-            p_a.orbital() # keep electrons from falling into nucleus
-
-
-
-
+                    em_interact(p_a, p_b)
+            p_a.orbital()  # keep electrons from falling into nucleus
 
     for p in particles:
         p.move()
